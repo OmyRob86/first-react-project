6749c38cbb98db4cf44bbd376949e45f73995d21
omy86 <harrysytito@gmail.com>
2020-04-08 11:55:51 +0200

    fix Home and add fetch on CreateArticle

--- src/pages/CreateArticle.js
+++ src/pages/CreateArticle.js
@@ -11,14 +11,31 @@ const CreateArticle = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log("titre : ", title);
-        console.log("content : ", content);
-        console.log("author : ", author);
-    }
+
+        fetch('http://localhost:3001/api/articles/create', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                title,
+                content,
+                author,
+            }),
+        })
+        .then((result) => {
+            return result.json();
+        })
+        .then(({ status }) => {
+            console.log(status);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    };
 
     const handleChange = (event) => {
-        console.log("target name : ", event.target.name);
-        console.log("target value : ", event.target.value);
 
         /*if (event.target.name === "title") {
             setTitle(event.target.value);
